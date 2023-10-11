106cca1aa1bb129e50f06527545024df8a4b6f02
hungdhv97 <hungdhv97@gmail.com>
2023-10-11 17:37:07 +0700

    Draft

--- story_site/settings.py
+++ story_site/settings.py
@@ -72,14 +72,14 @@ WSGI_APPLICATION = 'story_site.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Hung@230819971',
-        'HOST': 'db.eeqctqkrfdhksbilkrnp.supabase.co',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Hung@230819971',
+    #     'HOST': 'db.eeqctqkrfdhksbilkrnp.supabase.co',
+    #     'PORT': '5432',
+    # }
 }
 
 # Password validation
