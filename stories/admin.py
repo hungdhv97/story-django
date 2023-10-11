8151cb00dbdcd67e302b7790669c3f1a3382aafa
hungdhv97 <hungdhv97@gmail.com>
2023-10-11 16:45:55 +0700

    Big commit

---
+++ stories/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Author, Genre, Story, StoryGenre, Chapter, Rating, ReadingStats
+
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_cover_photo', 'description', 'author_id', 'created_date',
+                    'status', 'source', 'slug')
+
+    def display_cover_photo(self, obj):
+        if obj.cover_photo and hasattr(obj.cover_photo, 'url'):
+            return format_html(
+                '<img src="{}" width="100" />',
+                obj.cover_photo.url,
+            )
+        return "No cover photo"
+
+    display_cover_photo.short_description = 'Cover Photo'
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class StoryGenreAdmin(admin.ModelAdmin):
+    list_display = ('story_id', 'genre_id')
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'story_id', 'chapter_number', 'content', 'publish_date')
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'story_id', 'rating_value')
+
+
+class ReadingStatsAdmin(admin.ModelAdmin):
+    list_display = ('story_id', 'read_count', 'date')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Story, StoryAdmin)
+admin.site.register(StoryGenre, StoryGenreAdmin)
+admin.site.register(Chapter, ChapterAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(ReadingStats, ReadingStatsAdmin)
---
+++ stories/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.utils.text import slugify
+from unidecode import unidecode
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Story(models.Model):
+    class Status(models.TextChoices):
+        ONGOING = 'OG', 'Ongoing'
+        COMPLETED = 'CP', 'Completed'
+        DROPPED = 'DR', 'Dropped'
+
+    title = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_date = models.DateField()
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.ONGOING,
+    )
+    source = models.CharField(max_length=255, blank=True)
+    cover_photo = models.ImageField(upload_to='covers/', blank=True)
+    slug = models.SlugField(max_length=255, unique=True, editable=False, blank=True)
+
+    def save(self, *args, **kwargs):
+        original_slug = slugify(unidecode(self.title))
+        unique_slug = original_slug
+        num = 1
+
+        while Story.objects.filter(slug=unique_slug).exists():
+            unique_slug = f'{original_slug}-{num}'
+            num += 1
+
+        self.slug = unique_slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class StoryGenre(models.Model):
+    story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('story_id', 'genre_id'),)
+        indexes = [
+            models.Index(fields=['story_id', 'genre_id'], name='story_genre_idx'),
+        ]
+
+
+class Chapter(models.Model):
+    story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+    chapter_number = models.IntegerField()
+    content = models.TextField(blank=True)
+    publish_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.chapter_number}'
+
+
+class Rating(models.Model):
+    story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+    rating_value = models.IntegerField()
+
+
+class ReadingStats(models.Model):
+    story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+    read_count = models.IntegerField()
+    date = models.DateField()
+
+    class Meta:
+        unique_together = (('story_id', 'date'),)
+        indexes = [
+            models.Index(fields=['story_id', 'date'], name='reading_stats_idx'),
+        ]
