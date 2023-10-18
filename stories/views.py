6b5a39f857631310138729ae22b94fa04ace21ba
hungdhv97 <hungdhv97@gmail.com>
2023-10-18 15:51:37 +0700

    Refactor first api

--- stories/views.py
+++ stories/views.py
@@ -1,8 +1,9 @@
-from django.db.models import Count, Subquery, OuterRef, Sum, IntegerField, ExpressionWrapper, F, DurationField
-from django.db.models.functions import Now
-from rest_framework.generics import ListAPIView
+from django.db.models import Count, Sum, Case, When, Value, BooleanField, Avg
+from django.db.models.functions import Now, Round
 from django.utils import timezone
-from .models import Story, ReadingStats
+from rest_framework.generics import ListAPIView
+
+from .models import Story
 from .pagination import CustomPagination
 from .serializers import StorySerializer, StoryQueryParameterSerializer
 
@@ -14,42 +15,24 @@ class StoryListView(ListAPIView):
     def get_queryset(self):
         queryset = Story.objects.select_related("author")
 
-        # Deserialize query parameters
         param_serializer = StoryQueryParameterSerializer(data=self.request.query_params)
         param_serializer.is_valid(raise_exception=True)
         validated_data = param_serializer.validated_data
 
-        now = Now()
-
         queryset = queryset.annotate(
-            total_chapters=Count('chapter'),
-            # total_reads=Subquery(
-            #
-            #     ReadingStats.objects.filter(story=OuterRef('pk')).values('story')
-            #     .annotate(total=Sum('read_count')).values('total'),
-            #     output_field=IntegerField()
-            # ),
-            total_reads_2=Sum('readingstats__read_count', distinct=True),
-            # is_new=ExpressionWrapper(now - F('created_date'), output_field=DurationField()) <= timezone.timedelta(days=30),
-            # is_hot=Subquery(
-            #     ReadingStats.objects.filter(story=OuterRef('pk')).values('story')
-            #     .annotate(total=Sum('read_count')).values('total'),
-            #     output_field=models.IntegerField()
-            # ) >= 500,
-            # rating=Subquery(
-            #     Rating.objects.filter(story=OuterRef('pk')).values('story')
-            #     .annotate(avg_rating=Avg('rating_value')).values('avg_rating'),
-            #     output_field=models.FloatField()
-            # ),
-            # genres=Subquery(
-            #     Genre.objects.filter(storygenre__story=OuterRef('pk')).values('name'),
-            #     output_field=models.CharField()
-            # ),
-            # latest_chapter=Subquery(
-            #     Chapter.objects.filter(story=OuterRef('pk')).values('chapter_number')
-            #     .order_by('-chapter_number').values('chapter_number')[:1],
-            #     output_field=models.IntegerField()
-            # )
+            total_chapters=Count('chapter', distinct=True),
+            total_reads=Sum('readingstats__read_count', distinct=True),
+            is_new=Case(
+                When(created_date__gte=Now() - timezone.timedelta(days=30), then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField()
+            ),
+            is_hot=Case(
+                When(total_reads__gte=500, then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField()
+            ),
+            avg_rating=Round(Avg('rating__rating_value'), 2),
         )
 
         return queryset
