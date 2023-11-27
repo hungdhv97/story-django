b0493c20ac6e151bb8ef8c7844421fbd63b83491
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-11-27 18:15:51 +0700

    First settings

---
+++ story_scraper/management/commands/crawl.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from story_scraper.story_scraper import settings
+from story_scraper.story_scraper.spiders.story_spider import PropertiesSpider
+
+
+class Command(BaseCommand):
+    help = 'Release spider'
+
+    def handle(self, *args, **options):
+        crawler_settings = Settings()
+        crawler_settings.setmodule(settings)
+
+        process = CrawlerProcess(settings=crawler_settings)
+
+        process.crawl(PropertiesSpider)
+        process.start()
