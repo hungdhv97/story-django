da6e0f7b2a43897fe88b833525d6a7f7246de0ec
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-12-12 18:16:34 +0700

    update_pipeline

--- story_scraper/story_scraper/pipelines.py
+++ story_scraper/story_scraper/pipelines.py
@@ -5,10 +5,13 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from stories.models import Genre
 
 
 class StoryScraperPipeline:
+    def __init__(self):
+        Genre.objects.all().delete()
+
     def process_item(self, item, spider):
         item.save()
         return item
