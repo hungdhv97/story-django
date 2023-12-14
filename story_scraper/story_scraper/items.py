86ecfd47c2c94083841062fcf3ef59dfa8821781
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-12-14 15:20:23 +0700

    Add items

--- story_scraper/story_scraper/items.py
+++ story_scraper/story_scraper/items.py
@@ -5,8 +5,32 @@
 
 from scrapy_djangoitem import DjangoItem
 
-from stories.models import Genre
+from stories.models import Author, Story, StoryGenre, Chapter, Rating, ReadingStats, Genre
 
 
 class GenreItem(DjangoItem):
     django_model = Genre
+
+
+class AuthorItem(DjangoItem):
+    django_model = Author
+
+
+class StoryItem(DjangoItem):
+    django_model = Story
+
+
+class StoryGenreItem(DjangoItem):
+    django_model = StoryGenre
+
+
+class ChapterItem(DjangoItem):
+    django_model = Chapter
+
+
+class RatingItem(DjangoItem):
+    django_model = Rating
+
+
+class ReadingStatsItem(DjangoItem):
+    django_model = ReadingStats
