49e1c3522ed09345395fe53fea4b848a592750d4
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-12-15 16:45:10 +0700

    remove unused code

--- story_scraper/story_scraper/spiders/story_spider.py
+++ story_scraper/story_scraper/spiders/story_spider.py
@@ -2,7 +2,6 @@ import scrapy
 
 from stories.models import Author, Genre
 from story_scraper.story_scraper.const import MAX_PAGES
-from story_scraper.story_scraper.items import AuthorItem, GenreItem
 
 
 class StorySpider(scrapy.Spider):
@@ -41,36 +40,32 @@ class StorySpider(scrapy.Spider):
     def save_genre(self, response):
         genres = response.css('.col-truyen-main .info-holder .info a[itemprop="genre"]::text').getall()
         for genre in genres:
-            genre_item = GenreItem()
-            genre_item['name'] = genre.strip()
-            existing_author = Genre.objects.filter(name=genre_item['name']).first()
-            if existing_author is None:
-                genre_item.save()
+            existing_genre = Genre.objects.filter(name=genre.strip()).first()
+            if existing_genre is None:
+                Genre(name=genre).save()
 
     def save_author(self, response):
-        author_item = AuthorItem()
         author_name = response.css('.col-truyen-main .info-holder .info a[itemprop="author"]::text').get()
-        author_item['name'] = author_name.replace('\u200B', '').strip()
-        existing_author = Author.objects.filter(name=author_item['name']).first()
+        existing_author = Author.objects.filter(name=author_name.replace('\u200B', '').strip()).first()
         if existing_author is None:
-            author_item.save()
+            Author(name=author_name).save()
 
-    def save_story(self, response):
-        story_item = StoryItem()
-
-        # Extract story details
-        story_item['title'] = response.css('.story-title::text').get().strip()
-        story_item['description'] = response.css('.story-description::text').get().strip()
-
-        # Extract author
-        author_name = response.css('.author-name::text').get().strip()
-        author = Author.objects.filter(name=author_name).first()
-        story_item['author'] = author
-
-        # Extract and associate genres
-        genre_names = response.css('.genre-list .genre::text').getall()
-        genres = [Genre.objects.filter(name=gn.strip()).first() for gn in genre_names]
-        story_item['genres'] = genres
-
-        # Save the story item
-        story_item.save()
+    # def save_story(self, response):
+    #     story_item = Story()
+    #
+    #     # Extract story details
+    #     story_item['title'] = response.css('.story-title::text').get().strip()
+    #     story_item['description'] = response.css('.story-description::text').get().strip()
+    #
+    #     # Extract author
+    #     author_name = response.css('.author-name::text').get().strip()
+    #     author = Author.objects.filter(name=author_name).first()
+    #     story_item['author'] = author
+    #
+    #     # Extract and associate genres
+    #     genre_names = response.css('.genre-list .genre::text').getall()
+    #     genres = [Genre.objects.filter(name=gn.strip()).first() for gn in genre_names]
+    #     story_item['genres'] = genres
+    #
+    #     # Save the story item
+    #     story_item.save()
