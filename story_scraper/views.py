2d79b416b184242318b3e1611a01f9ea56e7966b
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-12-28 15:08:44 +0700

    Add css for crawl_stories.html

--- story_scraper/views.py
+++ story_scraper/views.py
@@ -7,10 +7,16 @@ from .forms import ListStoriesCrawlForm, SomeStoriesCrawlForm
 @staff_member_required
 def crawl_list_stories_view(request):
     form = ListStoriesCrawlForm()
+    for field in form.fields.values():
+        field.widget.attrs['class'] = ('shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 '
+                                       'leading-tight focus:outline-none focus:shadow-outline')
     return render(request, 'crawl_stories.html', {'form': form})
 
 
 @staff_member_required
 def crawl_some_stories_view(request):
     form = SomeStoriesCrawlForm()
+    for field in form.fields.values():
+        field.widget.attrs['class'] = ('shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 '
+                                       'leading-tight focus:outline-none focus:shadow-outline')
     return render(request, 'crawl_stories.html', {'form': form})
